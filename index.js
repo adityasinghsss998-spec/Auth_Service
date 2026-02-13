d0772220b77b1583b14159236a4e92fd92f62f66
aditya <adityasingh.sss998@gmail.com>
2026-02-14 01:57:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const {PORT} =require('./config/serverConfig')
+const app=express();
+const prepareAndStartServer = ()=> {
+  app.listen(PORT,()=> { 
+    console.log("server started on port",PORT)
+  })
+}
+prepareAndStartServer();
